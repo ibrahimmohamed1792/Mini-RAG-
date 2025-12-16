5030f51d737084259c1894119a38e468fc8ac584
Ibrahim Mohamed <ibrahim.mohammed1792@gmail.com>
2025-12-16 02:01:24 +0200

    modified:   src/main.py 	new file:   src/models/db_schemes/DataChunk.py 	new file:   src/models/db_schemes/__init__.py 	new file:   src/models/db_schemes/project.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient 
+from helpers.config import get_settings
 
 
 app = FastAPI()
+@app.on_event("starup")
+async def startup_dp_client():
+    settings=get_settings()
+
+    app.mongo_conn=AsyncIOMotorClient(settings.MONGODB_URL) # type: ignore 
+    app.db_client=app.mongo_conn[settings.MONGODB_DATABSE] # type: ignore 
+
+@app.on_event("shutdown")
+
+async def shutdown_db_client():
+    app.mongo_conn.close() # type: ignore
+
+
+
+
+
+
+
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
